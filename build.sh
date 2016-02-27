fd35b21600e44d951bf6f0588ea7d2adb37e5567
Kevin Upton <k_upton@live.com>
2016-02-27 22:23:21 +1000

    asasd

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ docker push tutum/lamp
 docker build -t kevs/app ./app
 docker push kevs/app
 
-docker rmi -f $(docker images -q -f "image-name=" )
+#docker rmi -f $(docker images -q -f "image-name=adasds" )
 
 ./start.sh
