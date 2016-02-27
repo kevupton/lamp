7140b126fc52484e8b6c64c97f5c5e5da7a8bd9d
Kevin Upton <k_upton@live.com>
2016-02-27 17:24:29 +1000

    asdsa

--- app/scripts.sh
+++ app/scripts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd /app
-#php artisan migrate:refresh --seed
-#vendor/bin/phpunit --configuration phpunit.xml
+php artisan migrate:refresh --seed
+vendor/bin/phpunit --configuration phpunit.xml
--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 docker build -t tutum/lamp ./lamp
 docker push tutum/lamp
 docker build -t kevs/app ./app
+docker push kevs/app
 
 ./start.sh
