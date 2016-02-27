83ab20e37cfbf9e4872cfdbb88771c3d43c05d3e
Kevin Upton <k_upton@live.com>
2016-02-27 22:22:08 +1000

    ssa

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 ./stop.sh
 
 docker rm $(docker ps -aq)
-docker rmi -f $(docker images -q)
 
 docker build -t tutum/lamp ./lamp
 docker push tutum/lamp
 docker build -t kevs/app ./app
 docker push kevs/app
+
+docker rmi -f $(docker images -q -f "image-name=" )
 
 ./start.sh
