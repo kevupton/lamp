f54ecea55ff0dc5d1f16c2545322100ec5af20d2
Kevin Upton <k_upton@live.com>
2016-02-27 18:10:10 +1000

    ssasda

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+./stop.sh
+
+docker rm $(docker ps -aq)
+docker rmi -f $(docker images -q)
+
 docker build -t tutum/lamp ./lamp
 docker push tutum/lamp
 docker build -t kevs/app ./app
